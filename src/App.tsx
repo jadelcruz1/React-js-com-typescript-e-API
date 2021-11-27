7bd58c9046f0724aa48762a8830328e706ecd64b
jadelcruz1 <jardelcruz6@gmail.com>
2021-11-27 20:29:10 -0300

    requisicao de api restfull funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const baseURL = 'https://psychonauts-api.herokuapp.com/api/characters?limit=50';
+
+
+
+const App:React.FC = () => {
+
+  useEffect(()=> {
+    axios.get(
+      'https://psychonauts-api.herokuapp.com/api/characters?limit=50;')
+      .then(response=> console.log(response.data))
+      .catch(err=> console.log(err));
+
+    },[]);
+
+      return<div> vou conseguir</div>;   
+  
+};
+
 
 export default App;
